a1b56856b7da903a1f4bc58b5886dfda4c585ac1
NS NooR <non377938@gmail.com>
2024-09-07 19:26:52 +0600

    Medico

---
+++ lib/view/screen/settings/widget/setting_list_card.dart
@@ -0,0 +1,37 @@
+import 'package:dr_apointment/view/common_widget/common_Text.dart';
+import 'package:flutter/material.dart';
+
+class SettingListViewCard extends StatelessWidget {
+  const SettingListViewCard({
+    super.key,
+    required this.screenHeight,
+    required this.screenWidth, required this.name, required this.onTap,
+  });
+
+  final double screenHeight;
+  final double screenWidth;
+  final String name;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+          height: screenHeight * 0.06,
+          width: screenWidth * 0.93,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(7),
+              color: Colors.white),
+          child: Center(child: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CommonText(text: name,fSize: 14,)
+              ],
+            ),
+          ))),
+    );
+  }
+}
