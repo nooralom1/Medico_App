1cd73eec52606d2365bb32860546bb6732044dbd
NS NooR <non377938@gmail.com>
2024-09-07 19:26:56 +0600

    Medico

---
+++ lib/view/screen/settings/settings.dart
@@ -0,0 +1,84 @@
+import 'package:dr_apointment/controller/getx_controller/faq.dart';
+import 'package:dr_apointment/view/common_widget/common_Text.dart';
+import 'package:dr_apointment/view/screen/bottom_navigation_bar.dart';
+import 'package:dr_apointment/view/screen/faq/widget/list_view_card.dart';
+import 'package:dr_apointment/view/screen/settings/widget/setting_list_card.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: screenHeight * 0.085,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.blue.shade100,
+          title: Image.asset("assets/logos/Group 8.png"),
+          centerTitle: true,
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Icon(Icons.notifications_active_outlined),
+            )
+          ],
+        ),
+        body: Container(
+          height: screenHeight,
+          width: screenWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.center,
+              colors: [
+                Colors.blue.shade100,
+                Colors.white,
+              ],
+            ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Get.off(() => const BottomNavBar());
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                        size: 27,
+                      )),
+                  const CommonText(
+                    text: "Setting",
+                    fSize: 18,
+                    fWeight: FontWeight.w600,
+                  )
+                ],
+              ),
+              SizedBox(height: screenHeight * 0.02),
+              SettingListViewCard(
+                screenHeight: screenHeight,
+                screenWidth: screenWidth,
+                name: 'Language',
+                onTap: () {},
+              ),
+              SizedBox(height: screenHeight*0.01,),
+              SettingListViewCard(
+                screenHeight: screenHeight,
+                screenWidth: screenWidth,
+                name: 'About',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
