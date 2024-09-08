0d0cbd70f05c2fab4bcf9e887c2b5ae0d215a3a7
NS NooR <non377938@gmail.com>
2024-09-08 21:15:02 +0600

    Medico

--- lib/controller/api_controller/faq_list.dart
+++ lib/controller/api_controller/faq_list.dart
@@ -4,13 +4,14 @@ import 'package:dr_apointment/database/faq_list.dart';
 import 'package:dr_apointment/model/faq_list.dart';
 import 'package:flutter/cupertino.dart';
 
-class FaqListService{
-  static Future<FaqListModel?> faqListService()async{
-    try{
+class FaqListService {
+  static Future<FaqListModel?> faqListService() async {
+    try {
       Future.delayed(const Duration(seconds: 2));
-      FaqListModel response = FaqListModel.fromJson(jsonDecode(jsonEncode(FaqList.faqList)));
+      FaqListModel response =
+          FaqListModel.fromJson(jsonDecode(jsonEncode(FaqList.faqList)));
       return response;
-    }catch(e){
+    } catch (e) {
       debugPrint("Error: $e");
     }
     return null;
