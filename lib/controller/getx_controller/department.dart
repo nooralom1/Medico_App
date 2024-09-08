fbe73b58c6bbe34cb2b5e095bf59965152e6858d
NS NooR <non377938@gmail.com>
2024-09-08 21:14:58 +0600

    Medico

--- lib/controller/getx_controller/department.dart
+++ lib/controller/getx_controller/department.dart
@@ -2,15 +2,14 @@ import 'package:dr_apointment/controller/api_controller/department_list.dart';
 import 'package:dr_apointment/model/department_list.dart';
 import 'package:get/get.dart';
 
-class DepartmentController extends GetxController{
-
+class DepartmentController extends GetxController {
   RxBool isLoading = false.obs;
   RxList<Departments> department = <Departments>[].obs;
 
-  getDepartments()async{
+  getDepartments() async {
     isLoading.value = true;
     var data = await DepartmentListService.departmentListService();
-    department.value = data?.departments??[];
+    department.value = data?.departments ?? [];
     isLoading.value = false;
   }
 
