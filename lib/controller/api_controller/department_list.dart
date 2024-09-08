1c47c906eb8680cef258dc53bf719afca0fe62ac
NS NooR <non377938@gmail.com>
2024-09-08 21:15:00 +0600

    Medico

--- lib/controller/api_controller/department_list.dart
+++ lib/controller/api_controller/department_list.dart
@@ -4,13 +4,14 @@ import 'package:dr_apointment/database/department_list.dart';
 import 'package:dr_apointment/model/department_list.dart';
 import 'package:flutter/cupertino.dart';
 
-class DepartmentListService{
-  static Future<DepartmentListModel?> departmentListService()async{
-    try{
+class DepartmentListService {
+  static Future<DepartmentListModel?> departmentListService() async {
+    try {
       Future.delayed(const Duration(seconds: 2));
-      DepartmentListModel response = DepartmentListModel.fromJson(jsonDecode(jsonEncode(DepartmentList.departmentList)));
+      DepartmentListModel response = DepartmentListModel.fromJson(
+          jsonDecode(jsonEncode(DepartmentList.departmentList)));
       return response;
-    }catch(e){
+    } catch (e) {
       debugPrint("Error: $e");
     }
     return null;
