4adbeafd28e9396be9b783ffc30edd3ed572ffc3
NS NooR <non377938@gmail.com>
2024-09-08 21:15:13 +0600

    Medico

--- lib/controller/getx_controller/faq.dart
+++ lib/controller/getx_controller/faq.dart
@@ -2,17 +2,17 @@ import 'package:dr_apointment/controller/api_controller/faq_list.dart';
 import 'package:dr_apointment/model/faq_list.dart';
 import 'package:get/get.dart';
 
-class FaqController extends GetxController{
-
+class FaqController extends GetxController {
   RxBool isLoading = false.obs;
   RxList<Faqs> faq = <Faqs>[].obs;
 
-  getFaqs()async{
-    isLoading.value=true;
+  getFaqs() async {
+    isLoading.value = true;
     var data = await FaqListService.faqListService();
-    faq.value = data?.faqs??[];
-    isLoading.value=false;
+    faq.value = data?.faqs ?? [];
+    isLoading.value = false;
   }
+
   @override
   void onInit() {
     getFaqs();
