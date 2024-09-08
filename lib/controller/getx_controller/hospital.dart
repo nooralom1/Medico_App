503c440be36062cfd0a13a31a52f1f6547cba25a
NS NooR <non377938@gmail.com>
2024-09-08 21:15:09 +0600

    Medico

--- lib/controller/getx_controller/hospital.dart
+++ lib/controller/getx_controller/hospital.dart
@@ -3,18 +3,18 @@ import 'package:dr_apointment/model/hospital_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class HospitalController extends GetxController{
-
+class HospitalController extends GetxController {
   TextEditingController searchController = TextEditingController();
   RxBool isLoading = false.obs;
-  RxList<Hospitals> hospital = <Hospitals> [].obs;
+  RxList<Hospitals> hospital = <Hospitals>[].obs;
 
-  getHospitalList()async{
+  getHospitalList() async {
     isLoading.value = true;
     var data = await HospitalListService.hospitalListService();
-    hospital.value = data?.hospitals??[];
-    isLoading.value= false;
+    hospital.value = data?.hospitals ?? [];
+    isLoading.value = false;
   }
+
   @override
   void onInit() {
     getHospitalList();
