2155ac6923bac1e6f999a170591f6b6d62ebaf34
NS NooR <non377938@gmail.com>
2024-09-08 21:15:04 +0600

    Medico

--- lib/controller/api_controller/investigation.dart
+++ lib/controller/api_controller/investigation.dart
@@ -3,13 +3,14 @@ import 'package:dr_apointment/database/investigation_list.dart';
 import 'package:dr_apointment/model/investigation_list.dart';
 import 'package:flutter/cupertino.dart';
 
-class InvestigationService{
-  static Future<InvestigationListModel?> investigationService()async{
-    try{
+class InvestigationService {
+  static Future<InvestigationListModel?> investigationService() async {
+    try {
       await Future.delayed(const Duration(seconds: 2));
-      InvestigationListModel response = InvestigationListModel.fromJson(jsonDecode(jsonEncode(InvestigationList.investigationList)));
+      InvestigationListModel response = InvestigationListModel.fromJson(
+          jsonDecode(jsonEncode(InvestigationList.investigationList)));
       return response;
-    }catch(e){
+    } catch (e) {
       debugPrint("Error: $e");
     }
     return null;
