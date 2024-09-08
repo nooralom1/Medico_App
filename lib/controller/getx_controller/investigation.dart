4174260cbd305fc9406a19cd8a85fb08c9ef470e
NS NooR <non377938@gmail.com>
2024-09-08 21:15:11 +0600

    Medico

--- lib/controller/getx_controller/investigation.dart
+++ lib/controller/getx_controller/investigation.dart
@@ -3,16 +3,15 @@ import 'package:dr_apointment/model/investigation_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class InvestigationController extends GetxController{
-
+class InvestigationController extends GetxController {
   TextEditingController searchController = TextEditingController();
   RxBool isLoading = false.obs;
   RxList<Investigations> investigation = <Investigations>[].obs;
 
-  getInvestigations()async{
+  getInvestigations() async {
     isLoading.value = true;
     var data = await InvestigationService.investigationService();
-    investigation.value = data?.investigations??[];
+    investigation.value = data?.investigations ?? [];
     isLoading.value = false;
   }
 
