195c85a915ac02c7973db32162a869ad93749742
NS NooR <non377938@gmail.com>
2024-09-08 21:16:11 +0600

    Medico

--- lib/database/investigation_list.dart
+++ lib/database/investigation_list.dart
@@ -1,30 +1,14 @@
-class InvestigationList{
-  static Map<String,dynamic> investigationList= {
+class InvestigationList {
+  static Map<String, dynamic> investigationList = {
     "investigations": [
-      {
-        "title": "Complete Blood Count (CBC)"
-      },
-      {
-        "title": "Basic Metabolic Panel (BMP)"
-      },
-      {
-        "title": "Vitamin D Test"
-      },
-      {
-        "title": "Liver Function Tests (LFTs)"
-      },
-      {
-        "title": "Thyroid Function Tests"
-      },
-      {
-        "title": "Hemoglobin A1c"
-      },
-      {
-        "title": "Prothrombin Time (PT)"
-      },
-      {
-        "title": "C-reactive Protein (CRP)"
-      }
+      {"title": "Complete Blood Count (CBC)"},
+      {"title": "Basic Metabolic Panel (BMP)"},
+      {"title": "Vitamin D Test"},
+      {"title": "Liver Function Tests (LFTs)"},
+      {"title": "Thyroid Function Tests"},
+      {"title": "Hemoglobin A1c"},
+      {"title": "Prothrombin Time (PT)"},
+      {"title": "C-reactive Protein (CRP)"}
     ]
   };
 }
