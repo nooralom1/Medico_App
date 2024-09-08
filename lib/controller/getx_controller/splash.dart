8c681e72116fa827caab0bd0cfc3c60dd6a779fa
NS NooR <non377938@gmail.com>
2024-09-08 21:15:06 +0600

    Medico

--- lib/controller/getx_controller/splash.dart
+++ lib/controller/getx_controller/splash.dart
@@ -1,9 +1,8 @@
 import 'package:dr_apointment/view/screen/welcome/welcome.dart';
 import 'package:get/get.dart';
 
-class SplashController extends GetxController{
-  
-  nextPageFun()async{
+class SplashController extends GetxController {
+  nextPageFun() async {
     await Future.delayed(const Duration(seconds: 3));
     Get.to(const Welcome());
   }
