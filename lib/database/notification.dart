731175be202efdc968c9f9b7e8a6dbb1b37e4807
NS NooR <non377938@gmail.com>
2024-09-09 20:02:20 +0600

    notification

---
+++ lib/database/notification.dart
@@ -0,0 +1,30 @@
+class NotificationList {
+  static Map<String, dynamic> notificationList = {
+    "notifications": [
+      {
+        "notification":
+            "Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum,"
+      },
+      {
+        "notification":
+            "Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum,"
+      },
+      {
+        "notification":
+            "Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum,"
+      },
+      {
+        "notification":
+            "Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum,"
+      },
+      {
+        "notification":
+            "Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum,"
+      },
+      {
+        "notification":
+            "Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum,"
+      }
+    ]
+  };
+}
