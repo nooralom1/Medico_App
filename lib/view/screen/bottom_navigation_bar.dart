fa1b862fa5cabc19e5cf64d57b314766546adcc7
NS NooR <non377938@gmail.com>
2024-09-21 22:03:27 +0600

    update

--- lib/view/screen/bottom_navigation_bar.dart
+++ lib/view/screen/bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:dr_apointment/controller/getx_controller/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
