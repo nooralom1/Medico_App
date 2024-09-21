832c80409f7044bf8e8258be480a53be526bc70c
NS NooR <non377938@gmail.com>
2024-09-21 22:02:51 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,4 @@
-import 'package:dr_apointment/view/screen/bottom_navigation_bar.dart';
-import 'package:dr_apointment/view/screen/change_language/change_language.dart';
-import 'package:dr_apointment/view/screen/department/department.dart';
-import 'package:dr_apointment/view/screen/doctor/doctor.dart';
-import 'package:dr_apointment/view/screen/doctor_info/doctor_info.dart';
-import 'package:dr_apointment/view/screen/doctor_report/doctor_report.dart';
-import 'package:dr_apointment/view/screen/faq/faq.dart';
-import 'package:dr_apointment/view/screen/home/home.dart';
-import 'package:dr_apointment/view/screen/hospital/hospital.dart';
-import 'package:dr_apointment/view/screen/investigation/investigation.dart';
-import 'package:dr_apointment/view/screen/notification/notification.dart';
-import 'package:dr_apointment/view/screen/settings/settings.dart';
 import 'package:dr_apointment/view/screen/splash/splash.dart';
-import 'package:dr_apointment/view/screen/support/support.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
